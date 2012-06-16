78e607151967a18afab9116ffd2d606b1ecbf187
Telofy <drescher@ferret-go.com>
2012-06-16 19:03:18 +0200

    Using setuptools and find_packages in setup.py

--- setup.py
+++ setup.py
@@ -1,50 +1,26 @@
 #! /usr/bin/python
-from distutils.core import setup
-
-NAME = 'feedgenerator'
-PACKAGES = ['feedgenerator', 'feedgenerator.utils', 'feedgenerator.contrib',
-        'feedgenerator.contrib.gis']
-DESCRIPTION = 'Standalone version of django.utils.feedgenerator'
-
-URL = "https://github.com/ametaireau/feedgenerator"
-
-CLASSIFIERS = ['Development Status :: 5 - Production/Stable',
-               'Environment :: Web Environment',
-               'Intended Audience :: Developers',
-               'License :: OSI Approved :: BSD License',
-               'Operating System :: OS Independent',
-               'Programming Language :: Python',
-               'Topic :: Internet :: WWW/HTTP',
-               'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-               'Topic :: Software Development :: Libraries :: Python Modules',
-]
-
-AUTHOR = 'Django Software Foundation'
-AUTHOR_EMAIL = 'foundation@djangoproject.com'
-MAINTAINER = 'Alexis Metaireau'
-MAINTAINER_EMAIL= 'alexis@notmyidea.org'
-KEYWORDS = "feed atom rss".split(' ')
-VERSION = '1.3.0'
+from setuptools import setup, find_packages
 
 setup(
-    name=NAME,
-    version=VERSION,
-    packages=PACKAGES,
-#    py_modules=MODULES,
-#    include_package_data = True,
-#    package_dir = {'feedgenerator': 'feedgenerator'},
-#    namespace_packages = ['feedgenerator'],
-#    install_requires=[
-#        'setuptools',
-#    ],
+    name='feedgenerator',
+    version='1.3.0',
+    packages=find_packages('.'),
 
-    # metadata for upload to PyPI
-    author=AUTHOR,
-    author_email=AUTHOR_EMAIL,
-    maintainer=MAINTAINER,
-    maintainer_email=MAINTAINER_EMAIL,
-    description=DESCRIPTION,
-    keywords=KEYWORDS,
-    url=URL,
-    classifiers=CLASSIFIERS,
+    author='Django Software Foundation',
+    author_email='foundation@djangoproject.com',
+    maintainer='Alexis Metaireau',
+    maintainer_email='alexis@notmyidea.org',
+    description='Standalone version of django.utils.feedgenerator',
+    keywords=('feed', 'atom', 'rss'),
+    url='https://github.com/ametaireau/feedgenerator',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules']
 )
